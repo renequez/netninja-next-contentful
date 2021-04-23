ff639b6b35d701ec65ad4ce4982b6119866ccd2c
renequez <ecristianbujor@gmail.com>
2021-04-23 21:06:57 +0300

    Initial commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,27 +1,31 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Layout({ children }) {
-  return (
-    <div className="layout">
-      <header>
-        <Link href="/">
-          <a>
-            <h1>
-              <span>Just Add</span>
-              <span>Marmite</span>
-            </h1>
-            <h2>Spread The Joy</h2>
-          </a>
-        </Link>
-      </header>
+	return (
+		<div className='layout'>
+			<header>
+				<Link href='/'>
+					<a>
+						<h1>
+							<span>Just Add</span>
+							<span>Marmite</span>
+						</h1>
+						<h2>Spread The Joy</h2>
+					</a>
+				</Link>
+			</header>
 
-      <div className="page-content">
-        { children }
-      </div>
+			<div className='page-content'>{children}</div>
 
-      <footer>
-        <p>Copyright 2021 Just Add Marmite :)</p>
-      </footer>
-    </div>
-  )
+			<footer>
+				<p>
+					© 2021{' '}
+					<Link href='/'>
+						<a>NetNinja Next Contentful Tutorial</a>
+					</Link>{' '}
+					- No rights are reserved. Do whatever you want with this.
+				</p>
+			</footer>
+		</div>
+	);
 }
